e90ee2c0365ec4fd158c4b4d616219d9f8965562
Bkzazzau1 <ibrahim.yahaya@miva.edu.ng>
2026-06-18 13:31:13 +0100

    Align smoke test with refreshed assessment title

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,6 +6,6 @@ void main() {
     await tester.pumpWidget(const StudentsUiDemoApp());
 
     expect(find.text('K-SLAS Student Portal'), findsWidgets);
-    expect(find.text('Mid-semester examination'), findsOneWidget);
+    expect(find.text('Mid-semester proctored examination'), findsOneWidget);
   });
 }
