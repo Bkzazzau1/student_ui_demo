8c649af87748af6f4df344c0eee62420f355748f
Bkzazzau1 <ibrahim.yahaya@miva.edu.ng>
2026-06-19 12:36:19 +0100

    Add optimized vision runtime Windows channel header

---
+++ windows/runner/optimized_vision_runtime_channel.h
@@ -0,0 +1,8 @@
+#ifndef RUNNER_OPTIMIZED_VISION_RUNTIME_CHANNEL_H_
+#define RUNNER_OPTIMIZED_VISION_RUNTIME_CHANNEL_H_
+
+#include <flutter/binary_messenger.h>
+
+void RegisterOptimizedVisionRuntimeChannel(flutter::BinaryMessenger* messenger);
+
+#endif  // RUNNER_OPTIMIZED_VISION_RUNTIME_CHANNEL_H_
