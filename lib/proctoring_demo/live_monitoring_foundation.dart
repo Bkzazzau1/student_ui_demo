17289bd8bc885c31db6e2f185c52bbbf86e561cb
Bkzazzau1 <ibrahim.yahaya@miva.edu.ng>
2026-06-22 21:57:47 +0100

    Add live monitoring foundation marker

---
+++ lib/proctoring_demo/live_monitoring_foundation.dart
@@ -0,0 +1,12 @@
+class LiveMonitoringFoundation {
+  const LiveMonitoringFoundation._();
+
+  static const String focus = 'pre_yolo_runtime_hardening';
+
+  static const List<String> completed = <String>[
+    'runtime_safety_policy',
+    'proctoring_risk_policy',
+    'live_event_retry_queue',
+    'evidence_event_models',
+  ];
+}
