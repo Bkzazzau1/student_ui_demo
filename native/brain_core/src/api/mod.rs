71a9cbf36df9e6f4f642a81670d101a33b97bbf3
Bkzazzau1 <ibrahim.yahaya@miva.edu.ng>
2026-06-25 09:14:20 +0100

    Register native lockdown API

--- native/brain_core/src/api/mod.rs
+++ native/brain_core/src/api/mod.rs
@@ -1,7 +1,9 @@
 pub mod evidence_vault;
+pub mod lockdown;
 pub mod proctoring;
 pub mod system_security;
 
 pub use evidence_vault::*;
+pub use lockdown::*;
 pub use proctoring::*;
 pub use system_security::*;
