844b230cd3a06affda2b8bda576b2643d5f19b70
Bkzazzau1 <ibrahim.yahaya@miva.edu.ng>
2026-06-29 08:50:54 +0100

    Update widget test for student login entry

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:students_ui_demo/main.dart';
 
 void main() {
-  testWidgets('student assessment gateway renders', (
-    WidgetTester tester,
-  ) async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() async {
+    await GetStorage.init();
+  });
+
+  testWidgets('student login screen renders', (WidgetTester tester) async {
     await tester.pumpWidget(const StudentsUiDemoApp());
 
     expect(find.text('K-SLAS Student Portal'), findsWidgets);
-    expect(find.text('Mid-semester proctored examination'), findsOneWidget);
+    expect(find.text('Sign in to continue'), findsOneWidget);
+    expect(find.text('Continue to student portal'), findsOneWidget);
   });
 }
