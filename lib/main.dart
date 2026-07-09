560008d6fe493328f5a9b53ec759e47165e60c37
Bkzazzau1 <ibrahim.yahaya@miva.edu.ng>
2026-07-09 09:59:10 +0100

    Keep Air Board route without global shortcut

--- lib/main.dart
+++ lib/main.dart
@@ -29,71 +29,7 @@ class StudentsUiDemoApp extends StatelessWidget {
       routes: <String, WidgetBuilder>{
         '/air-board': (_) => const AirBoardDemoView(),
       },
-      builder: (context, child) {
-        return Stack(
-          children: [
-            child ?? const SizedBox.shrink(),
-            Positioned(
-              right: 18,
-              bottom: 18,
-              child: SafeArea(
-                child: _AirBoardShortcut(
-                  onPressed: () => Navigator.of(context).pushNamed('/air-board'),
-                ),
-              ),
-            ),
-          ],
-        );
-      },
       home: const StudentLoginView(),
-    );
-  }
-}
-
-class _AirBoardShortcut extends StatelessWidget {
-  const _AirBoardShortcut({required this.onPressed});
-
-  final VoidCallback onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Colors.transparent,
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          color: const Color(0xFF0F4C81),
-          borderRadius: BorderRadius.circular(999),
-          boxShadow: const [
-            BoxShadow(
-              color: Color(0x260F172A),
-              blurRadius: 18,
-              offset: Offset(0, 10),
-            ),
-          ],
-        ),
-        child: InkWell(
-          borderRadius: BorderRadius.circular(999),
-          onTap: onPressed,
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 14, vertical: 11),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(Icons.border_color_outlined, color: Colors.white, size: 18),
-                SizedBox(width: 8),
-                Text(
-                  'Rough-work board',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w900,
-                    fontSize: 13,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
